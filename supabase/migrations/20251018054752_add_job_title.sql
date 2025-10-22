2c361fadcf0d0b307a47179d6cf277874ae3a57d
Stephen <stephentsui98@gmail.com>
2025-10-21 18:45:28 -0700

    Delete supabase/migrations/20251018054752_add_job_title.sql
    Delete incorrect migration file

--- supabase/migrations/20251018054752_add_job_title.sql
+++
@@ -1,8 +0,0 @@
-create table public.employees (
-  id bigint generated always as identity not null,
-  name text null,
-  email text null,
-  created_at timestamp with time zone null default now(),
-  job_title text null default 'NULL'::text,
-  constraint employees_pkey primary key (id)
-) TABLESPACE pg_default;
