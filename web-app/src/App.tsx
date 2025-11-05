6ebd5e88f0bfa02501bfa60048cfad152cf4b958
Ella Gordon <ellaggordon@gmail.com>
2025-11-04 18:15:17 -0800

    Added navigation pages

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,9 +1,14 @@
 // import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 import Events from './pages/Events';
-import Settings from './pages/Settings';
+import Create from './pages/Create'
+import Profile from './pages/Profile';
+import EventView from './pages/EventView';
+import Participants from './pages/Participants';
+import Teams from './pages/Teams';
+import Statistics from './pages/Statistics';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,14 +20,32 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/events">Events</Link>
           <Link to="/settings">Settings</Link>
+          <Link to="/create">Create</Link>
+
+          <Outlet />
         </nav>
 
         <Routes>
           <Route index element={<Home />} />
-          <Route path="events" element={<Events />} />
-          <Route path="settings" element={<Settings />} />
+          <Route path="profile" element={<Profile />} />
+          
+          <Route path="events" element={<Events />}>
+            <Route path="create" element={<Create />} />
+            <Route path="view-and-edit" element={<EventView />}>
+              <Route path="participants" element={<Participants />} />
+              <Route path="teams" element={<Teams />} />
+              <Route path="statistics" element={<Statistics />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<Home />} />
         </Routes>
-    </div>
+
+        {/* <Routes>
+          <Route index element={<LoginPage />} />
+          <Route path="create-account" element={<CreateAccount />} />
+        </Routes> */}
+      </div>
     </>
   )
 }
---
+++ web-app/src/pages/Participants.tsx
@@ -0,0 +1,9 @@
+export default function Participants() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Participants</h1>
+      <p>This is where organizations can view a list of participants for a specific event.</p>
+      {/* <a href=></a> */}
+    </div>
+  )
+}
