6355d06268cae884ea722e8ef06e08fdc859f72d
stephen1644 <stephentsui98@gmail.com>
2025-11-09 22:18:40 -0800

    commented out unused imports and routes

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,15 +1,16 @@
 // import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import './App.css'
 import TopNav from "./components/TopNav";
 import Events from './pages/Events';
 import Create from './pages/Create'
 import Profile from './pages/Profile';
-import EventView from './pages/EventView';
-import Participants from './pages/Participants';
-import Teams from './pages/Teams';
-import Statistics from './pages/Statistics';
-import Login from './pages/Login'; 
+// import EventView from './pages/EventView';
+// import Participants from './pages/Participants';
+// import Teams from './pages/Teams';
+// import Statistics from './pages/Statistics';
+// import Login from './pages/Login'; 
 
 const App: React.FC = () => {
   return (
--- web-app/src/components/TopNav.tsx
+++ web-app/src/components/TopNav.tsx
@@ -1,11 +1,13 @@
-import { Link, useLocation } from "react-router-dom";
+// import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuTrigger,
-  NavigationMenuContent
+//   NavigationMenuTrigger,
+//   NavigationMenuContent
 } from "@/components/ui/navigation-menu";
 
 const TopNav: React.FC = () => {
